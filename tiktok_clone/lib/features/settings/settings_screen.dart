1be5f83dc099244cc515f7aa8c1c9a92e7e72302
2rebee741 <2rebee741@gmail.com>
2023-05-05 08:07:43 +0900

    0505

---
+++ tiktok_clone/lib/features/settings/settings_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notification = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notification = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Setting"),
+      ),
+      body: ListView(
+        children: [
+          CupertinoSwitch(
+              value: _notification, onChanged: _onNotificationsChanged),
+          Switch(value: _notification, onChanged: _onNotificationsChanged),
+          Switch.adaptive(
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+          ),
+          SwitchListTile.adaptive(
+            title: const Text("enable list"),
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+            subtitle: const Text("sub option"),
+          ),
+          Checkbox(value: _notification, onChanged: _onNotificationsChanged),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+            title: const Text(
+              "Enable notifications",
+            ),
+          ),
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2030));
+              // print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              // print(date);
+              final booking = await showDateRangePicker(
+                  builder: (context, child) {
+                    return Theme(
+                      data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                      child: child!,
+                    );
+                  },
+                  context: context,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2030));
+              // print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          ListTile(
+            title: const Text("Logout(ios)"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoDialog(
+                context: context,
+                builder: (context) => CupertinoAlertDialog(
+                  title: const Text("Are you sure?"),
+                  content: const Text("please don't"),
+                  actions: [
+                    CupertinoDialogAction(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("no"),
+                    ),
+                    CupertinoDialogAction(
+                      onPressed: () => Navigator.of(context).pop(),
+                      isDefaultAction: true,
+                      child: const Text("YES"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("Logout action sheet bottom"),
+            textColor: Colors.red,
+            onTap: () {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (context) => CupertinoActionSheet(
+                  title: const Text("Are you sure?"),
+                  message: const Text("pls"),
+                  actions: [
+                    CupertinoActionSheetAction(
+                      isDefaultAction: true,
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text("NO"),
+                    ),
+                    CupertinoActionSheetAction(
+                      onPressed: () => Navigator.of(context).pop(),
+                      isDestructiveAction: true,
+                      child: const Text("YES"),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          const AboutListTile(
+            applicationVersion: "1.0",
+            applicationLegalese: "dont copy me ",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictokclone/constants/gaps.dart';
 import 'package:tictokclone/constants/sizes.dart';
+import 'package:tictokclone/features/settings/settings_screen.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -9,100 +12,346 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar(
-          floating: true,
-          // pinned: true,
-          stretch: true,
-          backgroundColor: Colors.teal,
-          collapsedHeight: 60,
-          expandedHeight: 40,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground
-            ],
-            title: Text("Profile"),
-          ),
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: const [
-              CircleAvatar(
-                backgroundColor: Colors.red,
-                radius: 20,
-              ),
-            ],
-          ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 10,
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("titem $index"),
-              ),
-            ),
-          ),
-          itemExtent: 100,
-        ),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          pinned: true,
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50,
-            (context, index) => Container(
-              color: Colors.blue[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("titem $index"),
-              ),
-            ),
-          ),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(Object context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.indigo,
-      child: const FractionallySizedBox(
-        heightFactor: 1,
-        child: Center(
-          child: Text(
-            "Title",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
+  void _onGearPressed() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const SettingsScreen(),
       ),
     );
   }
 
   @override
-  double get maxExtent => 100;
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              title: const Text("Profile_FA"),
+              actions: [
+                IconButton(
+                  onPressed: _onGearPressed,
+                  icon: const FaIcon(
+                    FontAwesomeIcons.gear,
+                    size: Sizes.size20,
+                  ),
+                )
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    radius: 50,
+                    foregroundColor: Colors.amber,
+                    foregroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1682847724222-207d5e8e0b97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
+                  ),
+                  Gaps.v20,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "@username",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: Sizes.size18,
+                        ),
+                      ),
+                      Gaps.h5,
+                      FaIcon(
+                        FontAwesomeIcons.solidCircleCheck,
+                        size: Sizes.size16,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                  Gaps.v12,
+                  SizedBox(
+                    height: Sizes.size44,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            const Text(
+                              "97",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size18,
+                              ),
+                            ),
+                            Gaps.v3,
+                            Text(
+                              "Following",
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        VerticalDivider(
+                          width: 32,
+                          thickness: Sizes.size1,
+                          color: Colors.grey.shade400,
+                          indent: Sizes.size14,
+                          endIndent: Sizes.size14,
+                        ),
+                        Column(
+                          children: [
+                            const Text(
+                              "10M",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size18,
+                              ),
+                            ),
+                            Gaps.v3,
+                            Text(
+                              "Followers",
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        VerticalDivider(
+                          width: 32,
+                          thickness: Sizes.size1,
+                          color: Colors.grey.shade400,
+                          indent: Sizes.size14,
+                          endIndent: Sizes.size14,
+                        ),
+                        Column(
+                          children: [
+                            const Text(
+                              "169.34M",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size18,
+                              ),
+                            ),
+                            Gaps.v3,
+                            Text(
+                              "Likes",
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Gaps.v40,
+                  FractionallySizedBox(
+                    widthFactor: 0.66,
+                    child: Row(
+                      children: [
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 4,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: Sizes.size12,
+                              horizontal: Sizes.size20,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(
+                                  Sizes.size4,
+                                ),
+                              ),
+                            ),
+                            child: const Text(
+                              "Follow",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                        Gaps.h3,
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: Sizes.size9,
+                              horizontal: Sizes.size11,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey.shade400,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(Sizes.size4),
+                              ),
+                            ),
+                            child: FaIcon(
+                              FontAwesomeIcons.youtube,
+                              size: Sizes.size20,
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
+                        ),
+                        Gaps.h3,
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: Sizes.size9,
+                              horizontal: Sizes.size11,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey.shade400,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(Sizes.size4),
+                              ),
+                            ),
+                            child: FaIcon(
+                              FontAwesomeIcons.caretDown,
+                              size: Sizes.size20,
+                              color: Colors.grey.shade800,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Gaps.v14,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: Sizes.size32,
+                    ),
+                    child: Text(
+                      "All highlight and where to watch livbe matches on FIFA+++",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Gaps.v14,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      FaIcon(
+                        FontAwesomeIcons.link,
+                        size: Sizes.size18,
+                      ),
+                      Gaps.h4,
+                      Text(
+                        "http://nomad coders.co.kr ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gaps.v20,
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.symmetric(
+                        horizontal: BorderSide(
+                          color: Colors.grey.shade200,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                    child: const TabBar(
+                      labelColor: Colors.black,
+                      labelPadding: EdgeInsets.symmetric(
+                        vertical: Sizes.size20,
+                      ),
+                      indicatorColor: Colors.black,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      tabs: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Sizes.size20,
+                          ),
+                          child: Icon(
+                            Icons.grid_4x4_rounded,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Sizes.size20,
+                          ),
+                          child: FaIcon(
+                            FontAwesomeIcons.heart,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          children: [
+            GridView.builder(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
 
-  @override
-  double get minExtent => 80;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
+              itemCount: 20,
+              padding: EdgeInsets.zero,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size2,
+                  mainAxisSpacing: Sizes.size2,
+                  childAspectRatio: 9 / 16),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  Stack(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: "lib/assets/images/lotto.png",
+                          image:
+                              "https://images.unsplash.com/photo-1682348686716-9a71d77e681c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 1,
+                        left: 1,
+                        child: Row(
+                          children: const [
+                            FaIcon(
+                              FontAwesomeIcons.circlePlay,
+                              size: Sizes.size18,
+                              color: Colors.white,
+                            ),
+                            Gaps.h2,
+                            Text(
+                              "4.1M",
+                              style: TextStyle(
+                                fontSize: Sizes.size18,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // Image.asset("lib/assets/images/lotto.png"),
+            ),
+            const Center(
+              child: Text("pagestwo "),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
