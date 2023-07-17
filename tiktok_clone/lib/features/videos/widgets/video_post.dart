0927b2b14de30505f094dde6a8a6bc6eebe02477
2rebee741 <2rebee741@gmail.com>
2023-07-17 21:42:24 +0900

    provider end

--- tiktok_clone/lib/features/videos/widgets/video_post.dart
+++ tiktok_clone/lib/features/videos/widgets/video_post.dart
@@ -2,9 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:tictokclone/common/widgets/video_configuration/video_config.dart';
 import 'package:tictokclone/constants/gaps.dart';
 import 'package:tictokclone/constants/sizes.dart';
+import 'package:tictokclone/features/videos/view_models/playback_config_vm.dart';
 import 'package:tictokclone/features/videos/widgets/video_button.dart';
 import 'package:tictokclone/features/videos/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
@@ -67,6 +67,10 @@ class _VideoPostState extends State<VideoPost>
       value: 1.5,
       duration: _animationDuration,
     );
+
+    context
+        .read<PlaybackConfigViewModel>()
+        .addListener(_onPlaybackConfigChanged);
   }
 
   @override
@@ -76,12 +80,25 @@ class _VideoPostState extends State<VideoPost>
     //widget.onVideoFinished(f;
   }
 
+  void _onPlaybackConfigChanged() {
+    if (!mounted) return;
+    final muted = context.read<PlaybackConfigViewModel>().muted;
+    if (muted) {
+      _videoPlayerController.setVolume(0);
+    } else {
+      _videoPlayerController.setVolume(1);
+    }
+  }
+
   void _onVisibilityChanged(VisibilityInfo info) {
     if (!mounted) return;
     if (info.visibleFraction == 1 &&
         !_isPaused &&
         !_videoPlayerController.value.isPlaying) {
-      _videoPlayerController.play();
+      final autoplay = context.read<PlaybackConfigViewModel>().autoplay;
+      if (autoplay) {
+        _videoPlayerController.play();
+      }
     }
     if (_videoPlayerController.value.isPlaying && info.visibleFraction == 0) {
       _onTogglePause();
@@ -174,11 +191,15 @@ class _VideoPostState extends State<VideoPost>
             left: 20,
             top: 40,
             child: IconButton(
-              icon: const FaIcon(false
+              icon: FaIcon(context.watch<PlaybackConfigViewModel>().muted
                   ? FontAwesomeIcons.volumeOff
                   : FontAwesomeIcons.volumeHigh),
               color: Colors.white,
-              onPressed: () {},
+              onPressed: () {
+                context
+                    .read<PlaybackConfigViewModel>()
+                    .setMuted(!context.read<PlaybackConfigViewModel>().muted);
+              },
             ),
           ),
           Positioned(
